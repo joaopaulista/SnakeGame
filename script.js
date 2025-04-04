dfd52fa238847ba80be43c7d3c6c87a7f11d52c7
joaopaulista <jvpaulistaa@gmail.com>
2025-04-04 16:58:01 -0300

    snake movement

--- script.js
+++ script.js
@@ -6,18 +6,41 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "down" //posicao inicial cobra
 
 function createBG() { //criar background
     context.fillStyle = "lightgreen"; //cores e estilo
     context.fillRect(0, 0, 16 * box, 16 * box); //local do jogo (RECT x, y, height, width)
 }
 
-function createSnake() {
+function createSnake() { //criar cobra
     for(i=0; i < snake.length; i++){
         context.fillStyle = "green"
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-createBG();
-createSnake();
+function startGame(){
+    createBG();
+    createSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+    //movimentacao cobra
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    //retira o ultimo elemento da array
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead); //acrescenta um elemento a frente
+}
+
+let game = setInterval(startGame, 100); //gerar o jogo
