ecb5f228cfe68474e3e3b1618eb5465c69747c78
joaopaulista <jvpaulistaa@gmail.com>
2025-04-04 17:24:00 -0300

    snake movement connected with keyboard

--- script.js
+++ script.js
@@ -20,6 +20,15 @@ function createSnake() { //criar cobra
     }
 }
 
+document.addEventListener('keydown', movement); //movimentacao cobra
+
+function movement(event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function startGame(){
     createBG();
     createSnake();
