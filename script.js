715ee25e27645ab71a8401db5602c2e600f90460
joaopaulista <jvpaulistaa@gmail.com>
2025-04-07 18:10:20 -0300

    possibility to eat the food

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d"); //plano 2d
 let box = 32; //pixels de cada quadrado
-let snake = []; //array tamanho cobra
+let snake = []; //array tamanho corpo cobra
 snake[0] = {
-    x: 8 * box,
+    x: 8 * box, //posicao inicial cobra
     y: 8 * box
 }
 let direction = "down" //posicao inicial cobra
+
+//posicao aleatoria para comida
 let food = {
     x: Math.floor(Math.random() * 15 + 1) * box,
     y: Math.floor(Math.random() * 15 +1) * box
@@ -14,7 +16,7 @@ let food = {
 
 function createBG() { //criar background
     context.fillStyle = "lightgreen"; //cores e estilo
-    context.fillRect(0, 0, 16 * box, 16 * box); //local do jogo (RECT x, y, height, width)
+    context.fillRect(0, 0, 16 * box, 16 * box); //local do jogo (RECT x, y, height, width) desenha fundo jogo
 }
 
 function createSnake() { //criar cobra
@@ -26,12 +28,13 @@ function createSnake() { //criar cobra
 
 function foodUp() {
     context.fillStyle = "red";
-    context.fillRect(food.x, food.y, box, box);
+    context.fillRect(food.x, food.y, box, box); //desenha comida
 }
 
-document.addEventListener('keydown', movement); //movimentacao cobra
+document.addEventListener('keydown', movement); //movimentacao cobra pelo teclado
 
 function movement(event){
+    //nao deixa a cobra de dar meia-volta
     if(event.keyCode == 37 && direction != "right") direction = "left";
     if(event.keyCode == 38 && direction != "down") direction = "up";
     if(event.keyCode == 39 && direction != "left") direction = "right";
@@ -39,7 +42,8 @@ function movement(event){
 }
 
 function startGame(){
-    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0; //borda infinita
+    //borda infinita
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
@@ -51,14 +55,21 @@ function startGame(){
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
-    //movimentacao cobra
+    //movimentacao cabeça cobra
     if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    //retira o ultimo elemento da array
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        //retira o ultimo elemento da array
+        snake.pop();
+    }
+    else {
+        //nova posicao para a comida
+        food.x = Math.floor(Math.random() * 15 +1) * box;
+        food.y = Math.floor(Math.random() * 15 +1) * box;
+    }
 
     let newHead = {
         x: snakeX,
@@ -68,4 +79,4 @@ function startGame(){
     snake.unshift(newHead); //acrescenta um elemento a frente
 }
 
-let game = setInterval(startGame, 100); //gerar o jogo
+let game = setInterval(startGame, 100); //gerar o jogo, atualiza
